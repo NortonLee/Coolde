d8e6064e4bea94c1b96c147d9316bcfe5b771ad2
Norton <lizard@yeah.net>
2015-04-14 13:12:23 +0800

    no mongodb current

--- app.js
+++ app.js
@@ -23,12 +23,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(session({
+/*app.use(session({
     secret: settings.cookieSecret,
     store: new MongoStore({
         db: settings.db
     })
-}));
+}));*/
 
 app.use('/', routes);
 
