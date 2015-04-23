6193f63250921e17628b9765485591226319bb89
Norton <lizard@yeah.net>
2015-04-23 23:03:53 +0800

    modify db settings

--- controllers/sign.js
+++ controllers/sign.js
@@ -1,6 +1,6 @@
 var models = require('../models');
 var crypto = require('crypto');
-var User = new models.User;
+var User = models.User;
 
 exports.showLogin = function(req, res){
     res.render('sign/login', {title: '登陆'});
@@ -10,8 +10,7 @@ exports.login = function(req, res){
     var md5 = crypto.createHash('md5');
     var password = md5.update(req.body.password).digest('base64');
     
-    User.findByName(req.body.username, function(err, user){
-        console.log(22222222);
+    User.findOne(req.body.username,function(err, user){
         if(err){
             console.log(err);
         }
--- models/user.js
+++ models/user.js
@@ -5,44 +5,8 @@ var userSchema = new mongoose.Schema({
     password: {type: String}
 });
 
-userSchema.statics.findByName = function(userName, callback) {
-    return this.findOne({userName: userName}, callback);
-}
-
-<<<<<<< HEAD
-module.exports = User;
-
-User.get = function get(username, callback){
-    MongoClient.connect(settings.url, function(err, db) { 
-        if (err)  
-        {  
-            console.log("the err is:");  
-            console.log(err);  
-        }  
-        else  
-        {  
-            db.collection('users', function(err, collection){
-                if(err){
-                    db.close();
-                    return callback(err);
-                }
-
-                collection.findOne({name: username}, function(err, doc){
-                    db.close();
-                    if(doc){
-                        var user = new User(doc);
-                        callback(err, user);
-                    }else{
-                        callback(err,null);
-                    }
-                });
-            });
-        }
-    });
-};
-=======
 mongoose.model('User', userSchema);
->>>>>>> origin/master
+
 
 
 
